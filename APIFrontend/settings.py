f90e5554728d12c8e98ca5613b08edfcba16084c
Farid <93608676+Versedcamel153@users.noreply.github.com>
2025-01-29 20:34:55 +0000

    Updated settings.py

--- APIFrontend/settings.py
+++ APIFrontend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^7lx%(^qo9+l%e^ld7^o&ny$@y8pg4axn5k)a-#n=*@ai_m&$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'quetzal-keen-infinitely.ngrok-free.app', 'https://e-shop-03jm.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'quetzal-keen-infinitely.ngrok-free.app', 'e-shop-03jm.onrender.com']
 
 
 # Application definition
